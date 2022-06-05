25aa556b4e7ba09657012cfa2161cc1a3b29d304
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-06-05 13:40:33 +0545

    simple comment added to explain

--- bootcamp/bootcamp-prep-main/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ bootcamp/bootcamp-prep-main/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,5 +1,11 @@
 // YOUR CODE BELOW
 const onlyOne = (a, b, c) => {
+  // && operater always executed from left to right that's why we are using
+  // && operater instead of "or" operater i.e "||"
+
+  // || this operater executed from right to left which is not convinent to
+  // this problem therefore we are not using it here
+
   if (!!a && !b && !c) {
     return true;
   }
